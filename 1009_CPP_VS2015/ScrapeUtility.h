cb62b7b32053bbc3e702beaf01e1b6fa284e6d0f
Vangeance666 <55984328+vangeance666@users.noreply.github.com>
2020-03-12 04:36:27 +0800

    Integrated libcurb which will most probable be used to get site HTMLs..

---
+++ 1009_CPP_VS2015/ScrapeUtility.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef SCRAPE_UTILITY_H
+#define SCRAPE_UTILITY_H
+
+#include "curl\curl.h"
+
+namespace ICT1009
+{
+	namespace Scrapper
+	{
+		class ScrapeUtility
+		{
+		public:
+			ScrapeUtility();
+			~ScrapeUtility();
+			void testFunction();
+		};
+	}
+}
+
+
+#endif
--- 1009_CPP_VS2015/main.cpp
+++ 1009_CPP_VS2015/main.cpp
@@ -1,13 +1,17 @@
 #include "UserInterfaceForm.h"
+#include "ScrapeUtility.h"
 
 using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
 int main() {
-	Application::EnableVisualStyles();
+	/*Application::EnableVisualStyles();
 	Application::SetCompatibleTextRenderingDefault(false);
 	My1009_CPP_VS2015::UserInterfaceForm form;
-	Application::Run(%form);
+	Application::Run(%form);*/
+	ICT1009::Scrapper::ScrapeUtility test;
+	test.testFunction();
+
 	return 0;
 }
